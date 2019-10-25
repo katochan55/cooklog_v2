9af806024ab69f1a6c50bc2de90b713f738c3d79
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-26 08:33:20 +0900

    Userテーブルにremember_digestカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_124216) do
+ActiveRecord::Schema.define(version: 2019_10_25_233042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_25_124216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
