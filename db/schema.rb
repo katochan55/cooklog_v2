66c6f61b706de0226a6d13d3c24cd54ae3c48294
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-25 21:43:37 +0900

    Usersテーブルにパスワードカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_123708) do
+ActiveRecord::Schema.define(version: 2019_10_25_124216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_10_25_123708) do
     t.string "sex"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
