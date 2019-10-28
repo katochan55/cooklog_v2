55cf43c91b188eec62699812ccae6e7a9279b28f
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-28 22:57:09 +0900

    トップページにフィード表示を追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @dish  = current_user.dishes.build
+      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
+    end
   end
 
   def about
