b3ba8cfe49c3c29642aa91cac750d549e01f86c8
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-29 08:13:22 +0900

    お料理登録機能を実装

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -9,6 +9,14 @@ before_action :logged_in_user, only: [:show, :new, :create, :edit, :update, :des
   end
 
   def create
+    @dish = current_user.dishes.build(dish_params)
+    if @dish.save
+      flash[:success] = "お料理が登録されました！"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
   end
 
   def edit
@@ -19,4 +27,10 @@ before_action :logged_in_user, only: [:show, :new, :create, :edit, :update, :des
 
   def destroy
   end
+
+  private
+
+    def dish_params
+      params.require(:dish).permit(:name, :discription, :portion, :tips, :reference, :required_time, :popularity)
+    end
 end
