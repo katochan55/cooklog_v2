8a1fc11235a91fd511ad28322c0b77d1f6aa1ad7
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-29 21:24:56 +0900

    Relationshipモデルを作成し、Userモデルとの関連付けを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_052509) do
+ActiveRecord::Schema.define(version: 2019_10_29_122024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2019_10_29_052509) do
     t.index ["user_id", "popularity"], name: "index_dishes_on_user_id_and_popularity"
     t.index ["user_id", "required_time"], name: "index_dishes_on_user_id_and_required_time"
     t.index ["user_id"], name: "index_dishes_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
