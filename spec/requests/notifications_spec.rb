468fe0e53438d32554fdb6832667d8149c8983fe
Kodai Kato <greatvoyage0122@gmail.com>
2019-10-31 10:11:30 +0900

    Notificationsコントローラ、ログイン制限付きindexアクション追加

---
+++ spec/requests/notifications_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "通知機能", type: :request do
+  let!(:user) { create(:user) }
+
+  context "通知一覧ページの表示" do
+    context "ログインしているユーザーの場合" do
+      it "レスポンスが正常に表示されること" do
+        login_for_request(user)
+        get notifications_path
+        expect(response).to render_template('notifications/index')
+      end
+    end
+
+    context "ログインしていないユーザーの場合" do
+      it "ログインページへリダイレクトすること" do
+        get notifications_path
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
+end
