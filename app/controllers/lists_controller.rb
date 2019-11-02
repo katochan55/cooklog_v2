6be2849466f92f4212e9f7aef27433701673be9f
Kodai Kato <greatvoyage0122@gmail.com>
2019-11-02 16:59:03 +0900

    クックログの追加をトップページ、マイページ、リスト一覧ページからもできるように実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,6 +3,7 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.where("user_id = ?", current_user.id)
+    @log = Log.new
   end
 
   def create
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @user = current_user
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
+      @log = Log.new
     else
       @user = User.new
     end
