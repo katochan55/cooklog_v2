776e5a775a4011c9d9eb02e9cc2f8def22050c25
Kodai Kato <greatvoyage0122@gmail.com>
2019-11-04 21:32:54 +0900

    icomoonフォントファイルを適切なフォルダに配置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -89,8 +89,10 @@ class UsersController < ApplicationController
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
-      flash[:danger] = "このページへはアクセスできません"
-      redirect_to(root_url) unless current_user?(@user)
+      if !current_user?(@user)
+        flash[:danger] = "このページへはアクセスできません"
+        redirect_to(root_url)
+      end
     end
 
     # 管理者ユーザーかどうか確認
